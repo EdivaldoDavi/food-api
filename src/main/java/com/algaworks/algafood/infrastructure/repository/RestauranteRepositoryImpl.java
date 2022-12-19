c9828062b01aa652f635e758696096daff3c8141
edivaldodavi <edidavi@gmail.com>
2022-12-19 10:39:40 -0300

    primeiro commit

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+
+@Component
+public class RestauranteRepositoryImpl {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Restaurante> listar() {
+		return manager.createQuery("from Restaurante", Restaurante.class)
+				.getResultList();
+	}
+	
+	public Restaurante buscar(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+
+	@Transactional
+	public Restaurante salvar(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+	
+	@Transactional
+	public void remover(Restaurante restaurante) {
+		restaurante = buscar(restaurante.getId());
+		manager.remove(restaurante);
+	}
+}
